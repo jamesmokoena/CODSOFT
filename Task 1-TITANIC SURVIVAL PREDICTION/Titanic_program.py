d0df7accfb0d46284765903ca563928c1efd5fd1
Ntethe James Mokoena <95544858+jamesmokoena@users.noreply.github.com>
2023-10-15 16:22:05 +0200

    added dataset files

--- Task 1-TITANIC SURVIVAL PREDICTION/Titanic_program.py
+++ Task 1-TITANIC SURVIVAL PREDICTION/Titanic_program.py
@@ -1,6 +1,31 @@
+
 import pandas as pd
+import numpy as np
+import seaborn as sns    
+from matplotlib import pyplot as plt
+from matplotlib import style
 
 
+# Algorithms
+from sklearn import linear_model
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import Perceptron
+from sklearn.linear_model import SGDClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC, LinearSVC
+from sklearn.naive_bayes import GaussianNB
 
 
-dataSet = 
+dataSet = pd.read_csv("./DATASET/test.csv")
+
+
+#print(dataSet)
+
+
+total = dataSet.isnull().sum().sort_values(ascending=False)
+percent_1 = dataSet.isnull().sum()/dataSet.isnull().count()*100
+percent_2 = (round(percent_1, 1)).sort_values(ascending=False)
+missing_data = pd.concat([total, percent_2], axis=1, keys=['Total', '%'])
+print(missing_data.head(5))
