2c50b21254fb49fbd8975d298a5207abcbb1967c
Ntethe James Mokoena <95544858+jamesmokoena@users.noreply.github.com>
2023-10-16 18:20:44 +0200

    done displaying relations

--- Task 1-TITANIC SURVIVAL PREDICTION/Titanic_program.py
+++ Task 1-TITANIC SURVIVAL PREDICTION/Titanic_program.py
@@ -6,12 +6,19 @@ import seaborn as sns
 sns.set()
 
 
-titanic_train = pd.read_csv('./DATASET/train.csv')
-titanic_test = pd.read_csv('./DATASET/test.csv')
+titanic_train = pd.read_csv('Task 1-TITANIC SURVIVAL PREDICTION/train.csv')
+titanic_test = pd.read_csv('Task 1-TITANIC SURVIVAL PREDICTION/test.csv')
 
-#print(titanic_train.shape)
+def Age_vs_Survival():
+    fig = plt.figure(figsize=(15,5))
+    ax1 = fig.add_subplot(131)
+    ax2 = fig.add_subplot(132)
+    ax3 = fig.add_subplot(133)
 
-sns.catplot(x='Pclass', y='Survived', hue='Sex', col='Embarked', data=titanic_train)
-#plt.show()
+    sns.violinplot(x="Embarked", y="Age", hue="Survived", data=titanic_train, split=True, ax=ax1)
+    sns.violinplot(x="Pclass", y="Age", hue="Survived", data=titanic_train, split=True, ax=ax2)
+    sns.violinplot(x="Sex", y="Age", hue="Survived", data=titanic_train, split=True, ax=ax3)
+    plt.show()
 
-
+if __name__ == "__main__":
+    Age_vs_Survival()
