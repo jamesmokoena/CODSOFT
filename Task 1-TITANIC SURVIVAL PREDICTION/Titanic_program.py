aa3c331f7a63478908d578c40fe2615b1fad744e
Ntethe James Mokoena <95544858+jamesmokoena@users.noreply.github.com>
2023-10-23 17:45:01 +0200

    .

--- Task 1-TITANIC SURVIVAL PREDICTION/Titanic_program.py
+++ Task 1-TITANIC SURVIVAL PREDICTION/Titanic_program.py
@@ -6,3 +6,5 @@ import seaborn as sns
 titanic_train = pd.read_csv('Task 1-TITANIC SURVIVAL PREDICTION/DATASET/train.csv')
 titanic_test = pd.read_csv('Task 1-TITANIC SURVIVAL PREDICTION/DATASET/test.csv')
 
+
+
